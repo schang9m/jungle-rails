89adc4d0bec9ef1c1b8dd1c9a67b03d7aea7ccd0
Sampson Chang <sampson.chang@gmail.com>
2024-08-22 16:43:05 -0700

    added authentication with user email & pw for login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,15 @@ class ApplicationController < ActionController::Base
   
   protect_from_forgery with: :exception
 
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+  helper_method :current_user
+
+  def authorize
+    redirect_to '/login' unless current_user
+  end
+  
   private
 
   def cart
@@ -20,11 +29,6 @@ class ApplicationController < ActionController::Base
     enhanced_cart.map {|entry| entry[:product].price_cents * entry[:quantity]}.sum
   end
   helper_method :cart_subtotal_cents
-
-  def formatted_currency(price)
-    format('$%.2f', price)
-  end
-  helper_method :formatted_currency
 
   def update_cart(new_cart)
     cookies[:cart] = {
