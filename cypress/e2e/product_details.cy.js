d4b62c8f3f837c68d22fbbac2fd313965ef8adb6
Sampson Chang <sampson.chang@gmail.com>
2024-08-27 16:48:28 -0700

    feature spec for product details

---
+++ cypress/e2e/product_details.cy.js
@@ -0,0 +1,24 @@
+describe("Home Page", () => {
+  it("should visit the home page", () => {
+    cy.visit("/");
+
+    // Add an assertion to verify the page has loaded. For example:
+    cy.title().should("include", "Jungle");
+  });
+
+  it("There is products on the page", () => {
+    cy.visit("/");
+    cy.get(".products article").should("be.visible");
+  });
+
+  it("should click on the first product on the page", () => {
+    cy.visit("/");
+
+    cy.get("article").first().click();
+
+    cy.url().should("include", "/products");
+
+    cy.get("h1").should("contain", "Scented Blade");
+    cy.get("span").contains("$24.99");
+  });
+});
